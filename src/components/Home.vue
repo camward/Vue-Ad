b5faff9f1208dc5960420c89982318ae5a85bdd4
alex <camward@yandex.ru>
2018-09-26 21:05:30 +0400

    загрузка объявлений

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,5 @@
 <template>
-
-  <div>
+  <div v-if="!loading">
     <v-container fluid>
       <v-layout row>
         <v-flex xs12>
@@ -49,6 +48,20 @@
       </v-layout>
     </v-container>
   </div>
+  <div v-else>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12 class="text-xs-center pt-5">
+          <v-progress-circular
+            indeterminate
+            :size="100"
+            :width="4"
+            color="primary"
+          ></v-progress-circular>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -59,6 +72,9 @@ export default {
     },
     ads () {
       return this.$store.getters.ads
+    },
+    loading () {
+      return this.$store.getters.loading
     }
   }
 }
@@ -71,7 +87,7 @@ export default {
     left: 50%;
     background: rgba(0, 0, 0, .5);
     transform: translate(-50%, 0);
-    padding: 5px 10px;
+    padding: 5px 15px;
     border-top-right-radius: 5px;
     border-top-left-radius: 5px;
   }
